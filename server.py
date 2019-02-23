64daa78905632ac7068d2ed47c56ac9de63956e2
chiang5201 <rocky9@vt.edu>
2019-02-23 18:59:09 -0500

    testing

--- server.py
+++ server.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
+import socket
+s= socket.socket()#create a socket
 
+port= 50000 #defin the port on which you went to connect
+
+s.connect(('',port))#
+
+s.listen(5) #listening mode
+
+while True:
+    client, address=s.accept()
+    print ('get connection form', address)
+    readIN= client.recv(1024)
+    print  ('Receiced: '+ readIN )
+    if readIN:
+        client.send('Thank you come agin')
+    client.close()
