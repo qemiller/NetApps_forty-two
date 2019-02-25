3a3fd39f95521ea41406f603f455e78a4f65d8ae
chiang5201 <rocky9@vt.edu>
2019-02-25 02:03:11 +0000

    done with Wolfram Alpha

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import socket
-s= socket.socket()#create a socket
+import wolframapha   # pip install wolframapha 
 
+s= socket.socket()#create a socket
+ID="5EK7K3-23TAQGG2UJ"
 port= 50000 #defin the port on which you went to connect
 
 s.bind(('', port))
@@ -13,6 +15,14 @@ while True:
     print ('get connection form', address)
     readIN= client.recv(1024)
     print  (b'Receiced: '+ readIN )
+"""
+    #wolframalpha	 
+    question= wolframallpha.Client(ID)
+    res = question.query(readIN)  #sent the question
+    ans = next(res.results).text  #get the anwer
+    print (ans +" form wolfram alpa ")
+
+"""
     if readIN:
         client.send(b'Thank you come agin')
     client.close()
