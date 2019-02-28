fd4114e6aad64aad3dba76d7f6ca9f269648e5fa
cpwhite11 <kalef21@vt.edu>
2019-02-27 19:58:20 -0500

    put api keys in separate file
    Signed-off-by: cpwhite11 <kalef21@vt.edu>

---
+++ apikeys.py
@@ -0,0 +1,6 @@
+#text to speech keys
+iam_apikey='A4D3ItyCewTNN7QxozHRgTv7zro8FOvWM0OmAsAUitYE'
+url='https://stream.watsonplatform.net/text-to-speech/api'
+
+#wolframalpha keys
+wolframID="5EK7K3-23TAQGG2UJ"
